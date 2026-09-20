116f9d05e887e6d820671df5fafedde6324d3976
khumo Charles Williams <st10456302@myemeris.edu.za>
2026-09-20 23:59:12 +0200

    integrate saved locations with REST API

--- CommuteCompanionApi/obj/Debug/net10.0/CommuteCompanionApi.AssemblyInfo.cs
+++ CommuteCompanionApi/obj/Debug/net10.0/CommuteCompanionApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommuteCompanionApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c68ee52eada6fe8afc330c8f07cb9767bd38b24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06733293f48726dfd2cc43cab68eb70183447bf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommuteCompanionApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommuteCompanionApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
